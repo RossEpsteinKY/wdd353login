5ac9c4e5c78671f1dd0b7cd22c774be6960f3bd0
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-06-16 13:03:38 -0400

    added appjs and server js initial code

---
+++ app/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
+
+//handle CORS
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','POST, PUT, PATCH, GET, DELETE');
+    }
+    next();
+});
+
+//EJS Middleware
+app.set('view-engine', 'ejs');
+app.engine('ejs', require('ejs').__express);
+
+//Static site middleware
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+//Error Handling Middleware
+app.use((req,res,next) => {
+    const error = new Error('NOT FOUND!!!');
+    error.status(404);
+    next(error);
+});
+
+app.use((error,req,res,next) =>{
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message,
+            status: error.status
+        },
+    });
+})
+
+
