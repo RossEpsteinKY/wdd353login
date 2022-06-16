3bca3b7e6a4b41bda051017de67ada96d46c7da0
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-06-16 13:11:25 -0400

    added app to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
-
 require('dotenv').config();
+const app = require('./app/app');
+
 
 http.createServer().listen(process.env.port,()=>{
    console.log(`Server now running on port: ${process.env.port}`);
