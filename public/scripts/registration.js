ca13f854cbfd6ea9100621f664eb0ae7aa3ba547
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-06-17 12:39:17 -0400

    backup

---
+++ public/scripts/registration.js
@@ -0,0 +1,56 @@
+let errors = {
+  firstName: {
+    error: true,
+    errorMsg: "First name is required"
+  },
+  lastName: {
+    status: "failed",
+    errorMsg: "Last name is required"
+  },
+  city: {
+    status: "failed",
+    errorMsg: "City is required"
+  },
+  state: {
+    status: "failed",
+    errorMsg: "State is required"
+  },
+  zip: {
+    status: "failed",
+    errorMsg: "Zip code is required"
+  },
+  age: {
+    status: "failed",
+    errorMsg: "Please select an age group"
+  },
+  gender: {
+    status: "failed",
+    errorMsg: "Please select your preferred gender option"
+  },
+  consent: {
+    status: "failed",
+    errorMsg: "Please accept the terms of the agreement"
+  },
+  bio: {
+    status: "failed",
+    errorMsg: "Please enter some information about yourself"
+  }
+}
+
+
+function checkForm() {
+   // The function returns the product of p1 and p2
+}
+
+exports.checkForm = function (req) {
+  console.log("CLICK");
+  console.log(req.body.firstName);
+  console.log('hits');
+  if(req.body.firstName.length == 0){
+      errors.firstName.error = true;
+      console.log('tacos');
+  }else {
+      console.log(req.body.firstName);
+  }
+}
+
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require("express/lib/response");
 const router = express.Router();
 
 router.get('/', (req,res) =>{
@@ -25,4 +26,33 @@ router.get('/registration', (req,res) =>{
     });
 });
 
+router.post('/registration', (req,res) =>{
+
+    var registrationCheck = require("../public/scripts/registration");
+    registrationCheck.checkForm(req);
+
+    // console.log('hits');
+    // if(req.body.firstName == ''){
+    //     registration.firstName.error = true;
+    // }else {
+    //     console.log(req.body.firstName);
+    // }
+    // const nameRequired = 'Name is required!';
+    // const emailRequired = 'Email is required!';
+    // const emailNotValid = 'Email is wrong!';
+    // const mobileRequired = 'Mobile is required!';
+
+
+
+    // console.log("the name is:  " + username);
+    //
+    // // Form.create(f,function(err,newlyCreatedForm){
+    // //     if(err)
+    // //     {
+    // //         console.log(err);
+    // //     }else{
+    // //         res.redirect("/result");
+    // //     }
+    // // });
+});
 module.exports = router;
