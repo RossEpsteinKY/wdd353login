b992e1ea9634188bd38f2eba2b7e3f7bcf4eaefe
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-06-17 09:00:59 -0400

    added registration js, and errors JSON object

---
+++ scripts/registration.js
@@ -0,0 +1,40 @@
+let errors = {
+  "firstName": {
+    "status": "failed",
+    "errorMsg": "First name is required"
+  },
+  "lastName": {
+    "status": "failed",
+    "errorMsg": "Last name is required"
+  },
+  "city": {
+    "status": "failed",
+    "errorMsg": "City is required"
+  },
+  "state": {
+    "status": "failed",
+    "errorMsg": "State is required"
+  },
+  "zip": {
+    "status": "failed",
+    "errorMsg": "Zip code is required"
+  },
+  "age": {
+    "status": "failed",
+    "errorMsg": "Please select an age group"
+  },
+  "gender": {
+    "status": "failed",
+    "errorMsg": "Please select your preferred gender option"
+  },
+  "consent": {
+    "status": "failed",
+    "errorMsg": "Please accept the terms of the agreement"
+  },
+  "bio": {
+    "status": "failed",
+    "errorMsg": "Please enter some information about yourself"
+  }
+}
+
+
