3708fdfcfa034a46195750a6e6edec85e431c9fe
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-06-22 10:16:11 -0400

    Added login form and login js file

---
+++ public/scripts/login.js
@@ -0,0 +1,18 @@
+let errors = {
+    userName: {
+        status: "failed",
+        errorMsg: "Your login information was incorrect"
+    },
+    password: {
+        status: "failed",
+        errorMsg: "Your login information was incorrect"
+    },
+}
+
+let loginInfo = {
+    username: 'Mike@aol.com',
+    Password: 'abc123'
+}
+
+errors.firstNameMsg = "First name is required";
+
